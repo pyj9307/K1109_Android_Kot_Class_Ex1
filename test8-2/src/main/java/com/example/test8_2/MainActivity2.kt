58244b8608fe5fd0db387d658db98a8cc0e6fcbb
pyjreact <iy1566@naver.com>
2023-03-13 09:10:13 +0900

    230310 7교시 수업 마무리

--- test8-2/src/main/java/com/example/test8_2/MainActivity2.kt
+++ test8-2/src/main/java/com/example/test8_2/MainActivity2.kt
@@ -10,11 +10,12 @@ import com.example.test8_2.databinding.ActivityMain2Binding
 
 
 class MainActivity2 : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
-
+    // 두번째 방법 실행 시 재정의해야하는 함수
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
         Log.d("lsy", "체크박스 클릭")
     }
 
+    // 익명함수 onCheckedChanged를 재정의
     class MyEventHandler : CompoundButton.OnCheckedChangeListener {
         override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
             Log.d("lsy", "체크박스 클릭")
@@ -26,7 +27,7 @@ class MainActivity2 : AppCompatActivity(), CompoundButton.OnCheckedChangeListene
         var imagestate : Int = 0
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
+//         먼저 바인딩을 해줘야 함(activity_main2.xml의 이름을 ActivityMain2로 바꿈
         val binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         // 3번째 SAM 기법(single abstract method)
@@ -63,7 +64,9 @@ class MainActivity2 : AppCompatActivity(), CompoundButton.OnCheckedChangeListene
 //        test.onCheckedChanged(binding.checkbox, true)
 
         // 1번째 방법
+//        val test = MyEventHandler()
 //        binding.checkbox.setOnClickListener{
+////             위에서 재정의된 onCheckedChanged를 불러씀
 //            test.onCheckedChanged(binding.checkbox,true)
 //        binding.checkbox.setOnCheckedChangeListener(MyEventHandler())
 //       }
