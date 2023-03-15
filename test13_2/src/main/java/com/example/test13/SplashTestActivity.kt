6cd4fc0edd3aba014f3723cf6053282fd481aa43
pyjreact <iy1566@naver.com>
2023-03-15 13:50:54 +0900

    230315 4교시 오류

---
+++ test13_2/src/main/java/com/example/test13/SplashTestActivity.kt
@@ -0,0 +1,19 @@
+package com.example.test13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_test)
+
+    Handler().postDelayed({
+        val intent = Intent(this@SplashTestActivity,MainTestActivity::class.java)
+        startActivity(intent)
+        finish()
+    },4000)
+}
+}
