3ff9b1dfbc20ee74adc3e4d1c4febcab60ccb0e6
pyjreact <iy1566@naver.com>
2023-03-16 16:41:47 +0900

    230316 7교시 저장 갤러리 사진 선택 후 파일 저장 및 불러오기 실습 test 하다가 카메라 켜기 작동안됨.

--- ch16_provider_test/src/main/java/com/example/myapplication/MainActivity.kt
+++ ch16_provider_test/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,16 @@
-package com.example.ch16_provider
+package com.example.myapplication
 
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import com.example.ch16_provider.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityMainBinding
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -83,7 +83,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        
+
         binding.cameraButton.setOnClickListener {
             //camera app......................
             //파일 준비...............
@@ -98,7 +98,7 @@ class MainActivity : AppCompatActivity() {
             filePath = file.absolutePath
             val photoURI: Uri = FileProvider.getUriForFile(
                 this,
-                "com.example.ch16_provider.fileprovider",
+                "com.example.myapplication.fileprovider",
                 file
             )
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
