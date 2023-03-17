3a4baa2600724959c03bd1ad1ebd5f2e2c9d6485
pyjreact <iy1566@naver.com>
2023-03-17 10:39:35 +0900

    230317 2교시 저장 ch16_provider_test_1갤러리 사진 선택 후 파일 저장 및 불러오기 실습, SQLite 조회, 쓰기 간단 실습

---
+++ ch16_provider_test_1/src/main/java/com/example/ch16_provider_test/FileUpload.kt
@@ -0,0 +1,15 @@
+package com.example.ch16_provider_test
+
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FileUpload {
+
+    companion object {
+
+    }
+}
